cd716f607d61b8685b0df5d94079903ee27daadc
kimkuno1026 <kengo1026@mju.ac.kr>
2026-04-25 23:57:37 +0900

    Update main.py

--- main.py
+++ main.py
@@ -22,20 +22,20 @@ print("-"*40)
 
 #
 expense_items = [] 
-print(f"일본에서 가장 돈이 많이 들것 같은 항목 3개를 입력하세요.(월세 제외)")
+print(f"월세를 제외한 일본에서 가장 돈이 많이 들것 같은 항목 3개를 입력하세요.")
 for i in range(3):
   item = input(f"{i+1}번째 항목: ")
   expense_items.append(item)
 
 expense_items.sort()
-expense_items.insert(0,"월세 (고정)")
+expense_items.insert(0,"월세")
 list_count = len(expense_items)
 
 print(f"{name}님의 경제 분석 리포트")
 print(f"현재 등록된 지출 관리 항목({list_count}개):{expense_items}")
 print("-"*40)
 
-#예산 등급 판정
+#예산 등급 판정 및 조언
 if total_jpy >= 300000:
   grade = "안정"
   msg = "한 달 이상 수입 없이도 안정적으로 생활하며 일자리를 고를 수 있습니다"
@@ -47,7 +47,7 @@ else :
   msg = "초기 자금을 더 확보 할 것을 권장합니다"
 
 
-#체류 기간 고려한 예산 등급 판정
+#체류 기간 고려한 예산 등급 판정 및 조언
 if grade == "안정" or grade == "보통" :
   if stay_months >= 6 and budget_krw >0 :
     advide = "자금 여유가 있으니 일본을 즐기며 알바를 시작하세요"
