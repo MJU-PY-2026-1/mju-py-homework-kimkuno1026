af45ee51c7a029eddb7fe06e732208c41ae168a7
kimkuno1026 <kengo1026@mju.ac.kr>
2026-04-26 00:40:39 +0900

    Update main.py

--- main.py
+++ main.py
@@ -25,6 +25,10 @@ expense_items = []
 print(f"월세를 제외한 일본에서 가장 돈이 많이 들것 같은 항목 3개를 입력하세요.")
 for i in range(3):
   item = input(f"{i+1}번째 항목: ")
+  if item == "" :
+    print("공백은 입력 할수 없습니다.다시 입력해주세요")
+    continue
+    
   expense_items.append(item)
 
 expense_items.sort()
